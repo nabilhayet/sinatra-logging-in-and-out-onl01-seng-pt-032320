22ca88de16665586a05e1754e254e92a71c4a91c
Nabil Hayet <nabil.bracu@gmail.com>
2020-06-26 22:35:34 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,12 +13,12 @@ class ApplicationController < Sinatra::Base
   post '/login' do
 
     @user = User.find_by(username: params[:username])
-    
+
     if @user
       session[:user_id] = @user.id
       redirect to '/account'
     else
-     redirect to '/error'
+     erb :error
     end
   end
 
