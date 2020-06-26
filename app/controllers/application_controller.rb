1c49a8b28b5d60118b27b9a0fb719a7c7aa0b250
Nabil Hayet <nabil.bracu@gmail.com>
2020-06-26 23:19:32 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,7 +23,7 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/account' do
-     if Helpers.current_user(session)
+     if Helpers.is_logged_in?(session)
        erb :account
      else
        erb :error
